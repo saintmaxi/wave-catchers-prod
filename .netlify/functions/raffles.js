a6fe61efce655a8208566d4633d18b23261e474c
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2022-03-21 21:46:54 -0500

    hm

--- .netlify/functions/raffles.js
+++ .netlify/functions/raffles.js
@@ -111,7 +111,7 @@ exports.handler = async function(event, context) {
          return {
             statusCode: 200,
             headers: { "content-type": "application/json" },
-            body: JSON.stringify(raffles)
+            body: JSON.stringify(raffles.currentRaffle)
           }
      
     } 
